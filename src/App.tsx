0a50edaabac7928f3a32ae9520929dfa3bb29d5b
Alexander Donato <somostrilia@gmail.com>
2026-03-27 18:07:18 -0300

    fix: adicionar rota gestao-fiscal no App.tsx (estava caindo no stub ModulePage)

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import CalendarioFinanceiro from "./pages/CalendarioFinanceiro";
 import DashboardSocio from "./pages/DashboardSocio";
 import RelatorioPorColaborador from "./pages/RelatorioPorColaborador";
 import GestaoUsuarios from "./pages/GestaoUsuarios";
+import GestaoFiscal from "./pages/GestaoFiscal";
 
 import CentroCustos from "./pages/CentroCustos";
 import ModuloEventos from "./pages/ModuloEventos";
@@ -80,6 +81,7 @@ const App = () => (
             <Route path="/empresa/:companyId/eventos" element={<ProtectedRoute><ModuloEventos /></ProtectedRoute>} />
             <Route path="/empresa/:companyId/relatorio-colaborador" element={<ProtectedRoute><RelatorioPorColaborador /></ProtectedRoute>} />
             <Route path="/empresa/:companyId/usuarios" element={<ProtectedRoute><GestaoUsuarios /></ProtectedRoute>} />
+            <Route path="/empresa/:companyId/gestao-fiscal" element={<ProtectedRoute><GestaoFiscal /></ProtectedRoute>} />
             <Route path="/empresa/:companyId/:moduleId" element={<ProtectedRoute><ModulePage /></ProtectedRoute>} />
             <Route path="*" element={<NotFound />} />
           </Routes>
